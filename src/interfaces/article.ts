08f12821b0ced9709dd4ea4ecc29e81e712ea979
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2023-12-31 09:21:07 -0600

    bugfix: update

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -2,8 +2,14 @@ export default interface Article {
   id: number;
   attributes: {
     title: string;
-    content: string;
     slug: string;
+    subtitle: string;
+    image: string;
+    excerpt: string;
+    sidebar: string;
+    support: string;
+    tags: string;
+    content: string;
     createdAt: string;
     updatedAt: string;
     publishedAt: string;
