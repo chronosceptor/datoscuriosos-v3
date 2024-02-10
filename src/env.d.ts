371f46a3569598c2661ce25c6d7b79aa7bac643b
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2024-02-10 10:34:02 -0600

    bugfix: update

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+declare module "astro-imagetools/components"
 interface ImportMetaEnv {
   readonly STRAPI_URL: string;
 }
